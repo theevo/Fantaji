3a1256c9357664ac5b8b0c7f7af333200afe8ba7
Theo Vora <theo.vora@gmail.com>
2023-11-29 16:19:53 -0800

    add lyrics of Fantasy by Meiko Nakahara

--- Sources/FantajiCommandLineToolFromXcode.swift
+++ Sources/FantajiCommandLineToolFromXcode.swift
@@ -9,6 +9,38 @@ import ArgumentParser
 @main
 struct FantajiCommandLineToolFromXcode: ParsableCommand {
     mutating func run() throws {
-        print("Hello, world!")
+        let str =
+"""
+[Intro]
+Woo!
+
+[Chorus]
+Koi wa puriisumu no fantasy (No fantasy)
+Dakara umareka wareru hazu
+Kako wa anata ni tsudzuku kaiten tobira (Anata)
+Yoru wa puriisumu no fantasy (No fantasy)
+Futari nanairo ni terashite
+Ima wa atarashii ude no naka de dancing
+(Mou dancing, mou dancing, mou dancing)
+
+[Verse 1]
+Hanaya ida tokai wo nukedashi
+Kuruma wa suberu no
+Hoshi no chiri bameta hearty lightou
+Futari no yoru ga hajimaru no
+Aa no toki, koi wo naku shite kara
+Fuyu no kirameki wo (Mou dancing)
+Wasururete itano ni
+
+[Chorus]
+Koi wa puriisumu no fantasy (No fantasy)
+Dakara umare kawareru hazu
+Kako wa anata ni tsudzuku kaiten tobira (Anata)
+Yoru wa puriisumu no fantasy (No fantasy)
+Futari nanairo ni terashite
+Ima wa atarashii ude no naka de dancing
+(Mou dancing, mou dancing, mou dancing)
+"""
+        print(str)
     }
 }
