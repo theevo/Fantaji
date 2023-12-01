529183380d854262747ab76f6da57efabefb0739
Theo Vora <theo.vora@gmail.com>
2023-11-30 21:44:34 -0800

    run with command `swift run fantaji`

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FantajiCommandLineToolFromXcode",
+    products: [
+        .executable(
+            name: "fantaji",
+            targets: ["FantajiCommandLineToolFromXcode"]
+        ),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
